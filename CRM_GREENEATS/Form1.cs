f4fafcc1360397e93fd8be1bc7350fe5386966a3
BernardoSoares303 <bbnado34@gmail.com>
2025-08-14 21:20:55 -0300

    tela incial!

--- CRM_GREENEATS/Form1.cs
+++ CRM_GREENEATS/Form1.cs
@@ -16,5 +16,15 @@ namespace CRM_GREENEATS
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox3_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
